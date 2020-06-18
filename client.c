f34950503c6d53df82dd09f067a0946df9a41961
Hein <hein@aoc.com.sg>
2020-06-18 17:51:11 +0800

    refactor the client server working code

--- client.c
+++ client.c
@@ -12,83 +12,68 @@
 
 int sockfd = 0;
 int reinit = 0;
-char server_ip[]="0.0.0.0";
+char server_ip[] = "0.0.0.0";
+char recvBuff[1024];
+
 void intHandler(int signal) {
     printf("intHanlder(): signal %d\n", signal);
     close(sockfd);
-    if(signal == 13) {
+    if (signal == 13) {
         reinit = 1;
     }
-    
-    if(signal == 2) {
+
+    if (signal == 2) {
         exit(signal);
     }
-    
+
 }
 
-int main(int argc, char *argv[])
-{
+void socket_connect(void) {
+    int ret = 0;
+    struct sockaddr_in serv_addr;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Error : Could not create socket \n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serv_addr, '0', sizeof (serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000);
+
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+        printf("\n inet_pton error occured\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ret = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0) {
+        printf("Error : Connect Failed, Error code: %d\n", ret);
+    }
+}
+
+int main(int argc, char *argv[]) {
     signal(SIGINT, intHandler);
     signal(SIGPIPE, intHandler);
     int n = 0;
-    char recvBuff[1024];
-    struct sockaddr_in serv_addr; 
 
-    memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
+    socket_connect();
 
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000); 
-
-    if(inet_pton(AF_INET, server_ip, &serv_addr.sin_addr)<=0)
-    {
-        printf("\n inet_pton error occured\n");
-        return 1;
-    } 
-    int ret;
-    if(ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-       printf("\n Error : Connect Failed, Error code: %d\n", ret);
-       return 1;
-    } 
-
-    while(1) {
-        while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-        {
+    while (1) {
+        while ((n = read(sockfd, recvBuff, sizeof (recvBuff) - 1)) > 0) {
             recvBuff[n] = 0;
-            printf("n: %d\n", n);
-            if(fputs(recvBuff, stdout) == EOF)
-            {
+            //printf("n: %d\n", n);
+            if (fputs(recvBuff, stdout) == EOF) {
                 printf("\n Error : Fputs error\n");
             }
         }
         close(sockfd);
-        if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        {
-            printf("\n Error : Could not create socket \n");
-            return 1;
-        }
-        if(ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        {
-           printf("\n Error : Connect Failed, Error code: %d\n", ret);
-           //return 1;
-        } 
-        printf("Read error code: %d\n", n);
+        socket_connect();
+        printf("Read error - code: %d\n", n);
         sleep(1);
     }
-   
-
-    printf("Received: %s\n", recvBuff);
-    if(n < 0)
-    {
+    if (n < 0) {
         printf("\n Read error \n");
-    } 
+    }
 
     return 0;
 }
--- server.c
+++ server.c
@@ -13,32 +13,27 @@
 int connfd = 0;
 int listenfd = 0;
 int reinit = 0;
+char sendBuff[1025];
+
 void intHandler(int signal) {
     printf("intHanlder(): signal %d\n", signal);
     close(listenfd);
     close(connfd);
-    
-    if(signal == 13) {
+
+    if (signal == 13) {
         reinit = 1;
     }
-    
-    if(signal == 2) {
+
+    if (signal == 2) {
         exit(signal);
     }
-    
+
 }
 
-int main(int argc, char *argv[]) {
-    signal(SIGINT, intHandler);
-    signal(SIGPIPE, intHandler);
-
-    int yes=1;        // For setsockopt() SO_REUSEADDR, below
-    
+void socket_listen(void) {
+    int yes = 1; // For setsockopt() SO_REUSEADDR, below
     struct sockaddr_in serv_addr;
-
-    char sendBuff[1025];
-    time_t ticks;
-
+    
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof (serv_addr));
     memset(sendBuff, '0', sizeof (sendBuff));
@@ -48,48 +43,41 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(5000);
 
     // Lose the pesky "address already in use" error message - ref: beej's guide
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-        
-    bind(listenfd, (struct sockaddr*) &serv_addr, sizeof (serv_addr));
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof (int));
 
-    listen(listenfd, 10);
+    bind(listenfd, (struct sockaddr*) &serv_addr, sizeof (serv_addr));
     printf("listening on socket\n");
-    
+    listen(listenfd, 10);
+
     connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
     printf("connection accepted\n");
+}
+
+int main(int argc, char *argv[]) {
+    signal(SIGINT, intHandler);
+    signal(SIGPIPE, intHandler);
+    
+    time_t ticks;
+    socket_listen();
+
     int ret;
     while (1) {
-        if(reinit == 1) {
-                reinit = 0;
-                printf("listening on socket\n");
-
-                listenfd = socket(AF_INET, SOCK_STREAM, 0);
-                memset(&serv_addr, '0', sizeof (serv_addr));
-                memset(sendBuff, '0', sizeof (sendBuff));
-
-                serv_addr.sin_family = AF_INET;
-                serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-                serv_addr.sin_port = htons(5000);
-
-                // Lose the pesky "address already in use" error message - ref: beej's guide
-                setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-
-                bind(listenfd, (struct sockaddr*) &serv_addr, sizeof (serv_addr));
-                listen(listenfd, 10);
-                connfd = accept(listenfd, (struct sockaddr*) NULL, NULL);
+        if (reinit == 1) {
+            reinit = 0;
+            socket_listen();
         }
-        
+
         ticks = time(NULL);
         snprintf(sendBuff, sizeof (sendBuff), "%.24s\r\n", ctime(&ticks));
         int buff_len = strlen(sendBuff);
-        printf("buffer length: %d\n", buff_len);
+        //printf("buffer length: %d\n", buff_len);
         ret = write(connfd, sendBuff, buff_len);
         if (ret < 0) {
             printf("Write Error - code: %d\n", ret);
-        }
-        else {
-            printf("Write Fun - Return code: %d\n", ret);
-        }
+        } 
+//        else {
+//            printf("Write Fun - Return code: %d\n", ret);
+//        }
         sleep(1);
     }
     printf("exiting program");
